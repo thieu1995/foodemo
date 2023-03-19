84fe9ccbbb1759cec3ca0b7d07bcb0e0a8226bc2
Thieu Nguyen <nguyenthieu2102@gmail.com>
2023-03-19 23:31:47 +0700

    Update math

--- foo/utils/__init__.py
+++ foo/utils/__init__.py
@@ -3,3 +3,5 @@
 #       Email: nguyenthieu2102@gmail.com            %                                                    
 #       Github: https://github.com/thieu1995        %                         
 # --------------------------------------------------%
+
+from .domath import calculate_perimeter_and_area_circle, calculate_perimeter_and_area_rectangle, calculate_perimeter_and_area_square
